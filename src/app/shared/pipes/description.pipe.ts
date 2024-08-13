752223d330714743cec8ac934eb43193ce60fd38
Gayathri Priyanka Ramesh <gayathripriyankaramesh@gmail.com>
2024-08-13 16:32:51 +0530

    Folder Structure Created

---
+++ src/app/shared/pipes/description.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'description',
+  standalone: true
+})
+export class DescriptionPipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return null;
+  }
+
+}
---
+++ src/app/shared/pipes/image.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'image',
+  standalone: true
+})
+export class ImagePipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return null;
+  }
+
+}
